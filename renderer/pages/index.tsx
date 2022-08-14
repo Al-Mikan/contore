fdad36f1aed7217fca32d63c15d14d32629eb81a
karintou8710 <karintou8710@gmail.com>
2022-08-14 23:35:42 +0900

    [fix] remove sample code

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,32 +1,10 @@
-import { useEffect } from 'react'
-import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => {
-  useEffect(() => {
-    const handleMessage = (_event, args) => alert(args)
-
-    // add a listener to 'message' channel
-    global.ipcRenderer.addListener('message', handleMessage)
-
-    return () => {
-      global.ipcRenderer.removeListener('message', handleMessage)
-    }
-  }, [])
-
-  const onSayHiClick = () => {
-    global.ipcRenderer.send('message', 'hi from next')
-  }
-
   return (
-    <Layout title="Home | Next.js + TypeScript + Electron Example">
-      <h1>Hello Next.js 👋</h1>
-      <button onClick={onSayHiClick}>Say hi to electron</button>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
+    <Layout title="Home | こんとれ！！">
+      <h1>はろー こんとれ！！</h1>
+      <button>やっほー</button>
     </Layout>
   )
 }
