3548870dc1dd26014386f66c24d43b101c9b35fd
karintou8710 <karintou8710@gmail.com>
2022-08-17 01:33:39 +0900

    [add] background image and concentrate page

--- renderer/components/Layout.tsx
+++ renderer/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react'
 import Head from 'next/head'
+import styled from 'styled-components'
 
 type Props = {
   children: ReactNode
@@ -7,14 +8,21 @@ type Props = {
 }
 
 const Layout = ({ children, title = 'こんとれ！！' }: Props) => (
-  <div>
+  <BackgroundImage>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     {children}
-  </div>
+  </BackgroundImage>
 )
+
+const BackgroundImage = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: url('/images/forest.jpg');
+  background-size: cover;
+`
 
 export default Layout
---
+++ renderer/pages/concentrate.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+const ConcentratePage = () => {
+  return (
+    <Layout title="集中画面 | こんとれ！！">
+      <h1>Hello</h1>
+    </Layout>
+  )
+}
+
+export default ConcentratePage
