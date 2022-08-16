fc3f05ca26aa0d64097146d1ba4fa289dc5b4581
karintou8710 <karintou8710@gmail.com>
2022-08-16 12:26:51 +0900

    [fix] type error of App, Stage

--- renderer/tests/sample.test.ts
+++ renderer/tests/sample.test.ts
@@ -1,3 +1,6 @@
+// cannot be compiled under '--isolatedModules を回避するためimportする
+import { sampleFunction } from '../utils/api'
+
 test('sample test', () => {
   expect('Hello,World').toBe('Hello,World')
 })
---
+++ renderer/utils/api.ts
@@ -0,0 +1,7 @@
+/* 
+   sample.test.tsのtype-checkが通るように作成
+   別の関数が追加されたら書き換える
+ */
+export const sampleFunction = () => {
+  return 0
+}
