a7cb49166509166c383237e0b2db1ca89737ddef
karintou8710 <karintou8710@gmail.com>
2022-08-21 01:42:02 +0900

    [add] jump animation

--- renderer/components/characters/MiniCat.tsx
+++ renderer/components/characters/MiniCat.tsx
@@ -15,26 +15,59 @@ const MiniCat = () => {
     '/img/mini-cat/3.png',
     '/img/mini-cat/2.png',
   ]
-
+  const defaultX = 900
+  const defaultY = 750
   const [moveTick, setMoveTick] = useState(0) // 提起的に移動を実行する
-  const [target, setTarget] = useState(0) // 移動先のX座標
-  const [nowP, setNowP] = useState(900) // 現在位置のX座標
+  const [targetX, setTargetX] = useState(defaultX) // 移動先のX座標
+  const [targetY, setTargetY] = useState(defaultY) // 移動先のY座標
+  const [nowX, setNowX] = useState(defaultX) // 現在位置のX座標
+  const [nowY, setNowY] = useState(defaultY) // 現在位置のY座標
   const [currentAnimation, setCurrentAnimation] = useState(0) // 現在のアニメーション
 
   /* 次の位置に移動する */
-  const next_pos = () => {
-    /* 0を基準に -move_w/4 <= x <= move_w/4で移動をする */
-    if (nowP == target) return
+  const next_posX = () => {
+    const speed = 2
+    if (nowX == targetX) return
 
-    const direction = (target - nowP) / Math.abs(target - nowP)
-    setNowP((prev) => prev + direction)
+    const directionX = (targetX - nowX) / Math.abs(targetX - nowX)
+    if (directionX > 0) {
+      /* -> */
+      setNowX((prev) => Math.min(prev + directionX * speed, targetX))
+    } else {
+      /* <- */
+      setNowX((prev) => Math.max(prev + directionX * speed, targetX))
+    }
+  }
+
+  const next_posY = () => {
+    const speed = 2
+
+    if (targetY == nowY) {
+      if (defaultY == nowY) {
+        /* 初期位置 */
+        return
+      } else {
+        /* ジャンプの頂上。落下を開始 */
+        setTargetY(defaultY)
+        return
+      }
+    }
+
+    const directionY = (targetY - nowY) / Math.abs(targetY - nowY)
+    if (directionY > 0) {
+      /* 落下中 */
+      setNowY((prev) => Math.min(prev + directionY * speed, targetY))
+    } else {
+      /* 上昇中 */
+      setNowY((prev) => Math.max(prev + directionY * speed, targetY))
+    }
   }
 
   /* clickでアニメーションを切り替え */
-  const switchAnimation = (target: 0 | 1) => {
+  const switchAnimation = (targetAnimation: 0 | 1) => {
     /* 基本型からのみ変更を許可 */
     if (currentAnimation !== BASIC_ANIMATION) return
-    setCurrentAnimation(target)
+    setCurrentAnimation(targetAnimation)
   }
 
   /* 基本アニメーション以外で使用 */
@@ -42,18 +75,31 @@ const MiniCat = () => {
     setCurrentAnimation(BASIC_ANIMATION)
   }
 
+  const startJump = () => {
+    const height = 50
+    /* 2段ジャンプは不可 */
+    if (nowY != defaultY) return
+    setTargetY(defaultY - height)
+  }
+
   useTick((_) => {
     /* durationごとにターゲットを変更 */
     const interval = 500
     if (moveTick == 0) {
-      setTarget(getRandomInt(500, 1100))
+      setTargetX(getRandomInt(500, 1100))
     }
-    next_pos()
+    next_posX()
+    next_posY()
     setMoveTick((prev) => (prev + 1) % interval)
 
     /* たまに瞬きをする */
     if (Math.random() <= 0.005) {
       switchAnimation(BLINK_ANIMATION)
+    }
+
+    /* たまにジャンプする */
+    if (Math.random() <= 0.001) {
+      startJump()
     }
   })
 
@@ -66,12 +112,12 @@ const MiniCat = () => {
         isPlaying={true}
         initialFrame={0}
         animationSpeed={0.05}
-        x={nowP}
-        y={750}
+        x={nowX}
+        y={nowY}
         scale={1.5}
         interactive={true}
         visible={currentAnimation == BASIC_ANIMATION}
-        pointerdown={() => switchAnimation(BLINK_ANIMATION)}
+        pointerdown={startJump} /* jump */
         containsPoint={containsPoint}
       />
       {/* 瞬き */}
@@ -81,8 +127,8 @@ const MiniCat = () => {
         isPlaying={currentAnimation == BLINK_ANIMATION}
         initialFrame={0}
         animationSpeed={0.1}
-        x={nowP}
-        y={750}
+        x={nowX}
+        y={nowY}
         scale={1.5}
         visible={currentAnimation == BLINK_ANIMATION}
         loop={false}
