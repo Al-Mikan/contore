ce7c7a62b0874b32a8524a3eae46a62094c9ff3a
Sippo <oppis02.anzu@gmail.com>
2022-08-26 23:20:44 +0900

    [add] home-display

---
+++ renderer/components/Bg.tsx
@@ -0,0 +1,15 @@
+import { Sprite } from "@inlet/react-pixi"
+
+const Bg = () => {
+    return (
+        <Sprite
+            image={"img/background.png"}
+            width={1920}
+            height={1080}
+            x={0}
+            y={0}
+    />
+    )
+}
+
+export default Bg 
--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,13 +1,48 @@
+import styled from 'styled-components'
+import { useRouter } from 'next/router'
+import { InteractionEvent } from 'pixi.js'
+
+import Canvas from '../components/Canvas'
 import Layout from '../components/Layout'
 import StartBtn from '../components/StartBtn'
+import { ST } from 'next/dist/shared/lib/utils'
+
+import { Sprite } from "@inlet/react-pixi"
+
+
+
+import Bg from '../components/Bg'
+import Bar from '../components/Bars'
+import MiniCat from '../components/characters/MiniCat'
+import Level from '../components/Level'
+import Heart from '../components/hearts'
 
 const IndexPage = () => {
+  const router = useRouter()
+  const handleStartClick: (event: InteractionEvent) => void = (event) => {
+    router.push('/concentrate')
+  }
+
   return (
     <Layout title="Home | こんとれ！！">
-      <h1>はろー こんとれ！！</h1>
-      <StartBtn></StartBtn>
+      <Container>
+        <Canvas> 
+          <Bg/>
+          <Bar n='1'/>
+          <StartBtn handleStartClick={handleStartClick}/>
+          <MiniCat isClickThrough={false}/>
+          <Level level="100"/>
+          <Heart/>
+        </Canvas>
+      </Container>
     </Layout>
   )
 }
+
+const Container = styled.div`
+  background-color: rgba(255, 255, 255, 1);
+  width: 100vw;
+  height: 100vh;
+`
 
 export default IndexPage
