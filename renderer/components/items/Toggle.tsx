a0eed5fe00d407a1084d59f67859f99e23e2b3c5
AlMikan <nekomimi987@gmail.com>
2022-09-01 16:52:40 +0900

    add toggle component

---
+++ renderer/components/items/Toggle.tsx
@@ -0,0 +1,38 @@
+import { Sprite } from '@inlet/react-pixi'
+import { InteractionEvent } from 'pixi.js'
+import { containsPointClickThrouth } from '../../utils/pixi_api'
+import { useState } from 'react'
+
+
+
+
+interface Props {
+    x?: number
+    y?: number
+    scale?: number
+    isToggle?:boolean
+    handleClick: (event: InteractionEvent) => void
+}
+
+const Toggle = ({ x = 0, y = 0, scale = 1 ,isToggle=false,handleClick}: Props ) => {
+  const [alpha, setAlpha] = useState(1)
+  const mouseover = () => setAlpha(0.8)
+  const mouseout = () => setAlpha(1)
+  return(
+    <Sprite
+    image={isToggle ? "/img/toggle/on.png" : "/img/toggle/off.png"}
+    x={x}
+    y={y}
+    scale={scale}
+    click={handleClick}
+    interactive={true}
+    alpha={alpha}
+    mouseover={mouseover}
+    mouseout={mouseout}
+    buttonMode={true}
+    containsPoint={containsPointClickThrouth}
+  />
+  )
+}
+
+export default Toggle
