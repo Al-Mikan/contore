1b806947a111128a2b985ca3ea8189eaff4221a0
AlMikan <nekomimi987@gmail.com>
2022-09-01 16:53:15 +0900

    add setting modal

---
+++ renderer/components/modals/SettingModal.tsx
@@ -0,0 +1,99 @@
+import { useState, useCallback } from 'react'
+import { Container, Sprite, Text } from '@inlet/react-pixi'
+import { InteractionEvent, TextStyle } from 'pixi.js'
+
+import { Position } from '../../types/character'
+import { containsPointClickThrouth } from '../../utils/pixi_api'
+import CloseBtn from '../buttons/CloseBtn'
+import Toggle from '../items/Toggle'
+
+interface Props {
+  x?: number
+  y?: number
+  scale?: number
+  handleClickToHome: (event: InteractionEvent) => void // Note: useRouterをResultModalから呼ぶとnullが返るのでpropsとして受け取る
+}
+
+const SettingModal = ({
+  x = 0,
+  y = 0,
+  scale = 1,
+  handleClickToHome,
+}: Props) => {
+  const [dragMode, setDragMode] = useState(false)
+  const [pos, setPos] = useState<Position>({ x: x, y: y })
+  const [beforeMousePos, setBeforeMousePos] = useState<Position>({ x: 0, y: 0 })
+
+  // ドラッグ操作
+  const mouseDown = (event: InteractionEvent) => {
+    const nx = event.data.global.x
+    const ny = event.data.global.y
+    setDragMode(true)
+    setBeforeMousePos({ x: nx, y: ny })
+  }
+
+  const mouseMove = (event: InteractionEvent) => {
+    if (!dragMode) return
+    /* currentTargetがnullのバグが発生したので条件分岐する */
+    if (event.currentTarget === null || event.currentTarget === undefined)
+      return
+    /* クリックした場所から移動した差だけ移動する */
+    /* nx,nyにセットすると、端っこをクリックすると始め瞬間移動するので必要 */
+    const nx = event.data.global.x
+    const ny = event.data.global.y
+    const currentCharacterPosX = event.currentTarget.x
+    const currentCharacterPosY = event.currentTarget.y
+    setPos({
+      x: currentCharacterPosX + (nx - beforeMousePos.x),
+      y: currentCharacterPosY + (ny - beforeMousePos.y),
+    })
+    setBeforeMousePos({ x: nx, y: ny })
+  }
+
+  const mouseUp = (event: InteractionEvent) => {
+    setDragMode(false)
+  }
+  //toggleの処理
+  let [isToggle, setIsToggle] = useState(true)
+
+  const handleToggleChange = useCallback(() => {
+    if(isToggle) {
+        setIsToggle(false)
+    }else{
+        setIsToggle(true)
+    }
+}, [isToggle])
+
+  return (
+    <Sprite
+      anchor={0.5}
+      image="/img/modal.png"
+      visible={true}
+      x={pos.x}
+      y={pos.y}
+      scale={scale}
+      interactive={true}
+      containsPoint={containsPointClickThrouth}
+      mousedown={mouseDown}
+      mousemove={mouseMove}
+      mouseup={mouseUp}
+      mouseupoutside={mouseUp}
+    >
+      <Toggle
+        handleClick={handleToggleChange}
+        x={0}
+        y={0}
+        scale={0.4}
+        isToggle={isToggle}
+      ></Toggle>
+      <CloseBtn
+        handleClick={handleClickToHome}
+        x={150}
+        y={-175}
+        scale={0.4}
+      ></CloseBtn>
+    </Sprite>
+  )
+}
+
+export default SettingModal
