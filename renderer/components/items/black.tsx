fd059625d17c28bc9029654d075401329d338716
Sippo <oppis02.anzu@gmail.com>
2022-09-11 03:39:49 +0900

    [style] yarn format

--- renderer/components/items/black.tsx
+++ renderer/components/items/black.tsx
@@ -1,16 +1,13 @@
 import { Sprite } from '@inlet/react-pixi'
-import {InteractionEvent} from "pixi.js"
+import { InteractionEvent } from 'pixi.js'
 import { containsPointClickThrouth } from '../../utils/PixiAPI'
-
 
 import { BasicSpriteProps } from '../../types/sprite'
 
 interface Props extends BasicSpriteProps {}
 
-
-
 const Black = ({ x, y, scale = 1 }: Props) => {
-    return (
+  return (
     <Sprite
       image="/static/img/black.png"
       x={0}
@@ -21,27 +18,27 @@ const Black = ({ x, y, scale = 1 }: Props) => {
       height={720}
       interactive={true}
       containsPoint={containsPointClickThrouth}
-      click={(e:InteractionEvent) => {
-        e.stopPropagation();
-    }}
-      mousedown={(e)=>{
-          e.stopPropagation()
-     }}
-      mousemove={(e)=>{
-          e.stopPropagation()
-     }}
-      mouseup={(e)=>{
-          e.stopPropagation()
-     }}
-      mouseout={(e)=>{
-          e.stopPropagation()
-     }}
-      mouseover={(e)=>{
-          e.stopPropagation()
-     }}
-      mouseupoutside={(e)=>{
-          e.stopPropagation()
-     }}
+      click={(e: InteractionEvent) => {
+        e.stopPropagation()
+      }}
+      mousedown={(e) => {
+        e.stopPropagation()
+      }}
+      mousemove={(e) => {
+        e.stopPropagation()
+      }}
+      mouseup={(e) => {
+        e.stopPropagation()
+      }}
+      mouseout={(e) => {
+        e.stopPropagation()
+      }}
+      mouseover={(e) => {
+        e.stopPropagation()
+      }}
+      mouseupoutside={(e) => {
+        e.stopPropagation()
+      }}
     />
   )
 }
