8b5c794d9d1b08961700a1519daa285a4c82dd26
karintou8710 <karintou8710@gmail.com>
2022-09-13 01:41:53 +0900

    [refactor] add LevelRing components

--- renderer/components/items/StatusBox.tsx
+++ renderer/components/items/StatusBox.tsx
@@ -2,37 +2,23 @@ import { Container } from '@inlet/react-pixi'
 import { useContext } from 'react'
 
 import { BasicSpriteProps } from '../../types/sprite'
-import ExperiencePoint from '../../utils/ExperiencePoint'
 import HealthPoint from '../../utils/HealthPoint'
 import { GameContext } from '../containers/CanvasContext'
 import Coin from './Coin'
 import CuteFish from './CuteFish'
-import Level from './Level'
-import LevelBar from './LevelBar'
+import LevelRing from './LevelRing'
 import LifeGauge from './LifeGauge'
 import NumText from './NumText'
 
 interface Props extends BasicSpriteProps {}
 
 const StatusBox = ({ x = 0, y = 0, scale = 1 }: Props) => {
-  const { coin, fish, experiencePoint, health } = useContext(GameContext)
-  const ex = new ExperiencePoint(experiencePoint)
+  const { coin, fish, health } = useContext(GameContext)
   const hp = new HealthPoint(health)
 
   return (
     <Container x={x} y={y} scale={scale}>
-      <Container>
-        <LevelBar n={ex.progress(8)} x={-60} y={-30} scale={0.75} />
-        <Level x={130} y={170} scale={0.7} />
-        <NumText
-          n={ex.get_level()}
-          view_digits={3}
-          x={80}
-          y={140}
-          scale={0.65}
-          is_headzero_displayed={true}
-        />
-      </Container>
+      <LevelRing x={-70} y={-40} scale={0.8} />
       <LifeGauge
         n={hp.get_health_point_formatted(10)}
         x={30}
